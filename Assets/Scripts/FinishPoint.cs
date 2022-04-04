42fb704d817cb9b87fba6829663e4c2a3f485998
Jey Bloody <bloodjey98@gmail.com>
2022-04-04 21:45:14 +0500

    Update: Base logic

---
+++ Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    [SerializeField] private bool isStart;
+
+    public bool IsStart => isStart;
+}
